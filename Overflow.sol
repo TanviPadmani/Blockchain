e32cf5a949c3fac13f151c12661aac54f3c9201f
TanviPadmani <106952905+TanviPadmani@users.noreply.github.com>
2022-09-02 12:07:18 +0530

    Add files via upload

---
+++ Overflow.sol
@@ -0,0 +1,12 @@
+// SPDX-License_Identifier; MIT
+
+pragma solidity ^0.6.0;
+
+
+contract Overflow{
+
+    function overflow() public view returns(uint8){
+        uint8 big = 255 + uint8(100);
+        return big;
+    }
+}
